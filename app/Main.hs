366bb2354427ec17e807aeba83efca5e18583e90
Konata <42537566+konata-chan404@users.noreply.github.com>
2024-01-10 05:36:49 +0200

    SSTV Header Implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,35 @@
 module Main where
 
-import Data.Int (Int32)
+import Data.Bits (testBit)
+import Data.Int (Int32, Int8)
 import Sound
 
--- Generating sample data
-samples :: [[Int32]]
-samples =
-  map
-    (: [])
-    ( tone 600 300 (volume 0.5)
-        ++ tone 0 200 (volume 0.5)
-        ++ tone 600 300 (volume 0.5)
-    )
+bit :: Bool -> [Int32]
+bit True = tone 1100 30
+bit False = tone 1300 30
+
+vox :: [Int32]
+vox =
+  tone 1900 100
+    ++ tone 1500 100
+    ++ tone 1900 300
+    ++ tone 1500 100
+    ++ tone 2300 100
+    ++ tone 1500 100
+    ++ tone 2300 100
+    ++ tone 1500 100
+
+header :: Int8 -> [Int32]
+header id =
+  tone 1900 300
+    ++ tone 0 10
+    ++ tone 1900 300
+    ++ tone 1200 30
+    ++ concatMap bit (reverse [testBit id i | i <- [0 .. 6]])
+    ++ bit (odd $ toInteger id)
+    ++ tone 1200 30
 
 -- Main function
 main :: IO ()
 main = do
-  makeWavFile "temp.wav" (waveData samples)
+  makeWavFile "temp.wav" (waveData (vox ++ header 60))
--- app/Sound.hs
+++ app/Sound.hs
@@ -5,27 +5,30 @@ import Data.WAVE
 
 -- Constants
 samplesPerSecond :: Int
-samplesPerSecond = 16000
+samplesPerSecond = 48000
+
+volumeGeneral :: Int32
+volumeGeneral =  (volume 0.8)
 
 bitrate :: Int
 bitrate = 32
 
 -- Function to generate tone samples
-tone :: Double  -> Double -> Int32 -> [Int32]
-tone freq len vol = toneSample  freq samplesPerSecond len vol
+tone :: Double  -> Double -> [Int32]
+tone freq len = toneFull freq samplesPerSecond len volumeGeneral
 
 -- Function to generate tone samples
-toneSample :: Double -> Int -> Double -> Int32 -> [Int32]
-toneSample freq samples len vol =
-  take (round $ (len/1000) * fromIntegral samples) $
+toneFull :: Double -> Int -> Double -> Int32 -> [Int32]
+toneFull freq samples len vol =
+  take (round $ len/1000 * fromIntegral samples) $
     map ((round . (* fromIntegral vol)) . sin) [0.0, (freq * 2 * pi / fromIntegral samples) ..]
 
 volume :: Double -> Int32
 volume v = maxBound `div` (round (v ** (-1)))
 
 -- Creating WAVE data
-waveData :: [[Int32]] -> WAVE
-waveData = WAVE (WAVEHeader 1 samplesPerSecond bitrate Nothing)
+waveData :: [Int32] -> WAVE
+waveData d = WAVE (WAVEHeader 1 samplesPerSecond bitrate Nothing) (map (:[]) d)
 
 -- Function to write WAVE data to a file
 makeWavFile :: FilePath -> WAVE -> IO ()
